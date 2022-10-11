e49816bd81f0787ae8a4fc5366ad5966dd82e6e9
aya-abdullah <ayalaban888@yahoo.com>
2022-10-11 18:33:27 +0200

    UPDATE

--- src/config/dbInit.js
+++ src/config/dbInit.js
@@ -1,16 +1,4 @@
 
-import mydatabase from "../constants/projects.json";
-import { db } from "./firebase";
-
-addData = async () => {
-    mydatabase.forEach(async (element) => {
-        element.tags = element.tags.map(tag=>`/tags/${tag}`);
-        const l = await firebase.firestore().collection('projects').doc(element.name).set(element);
-        console.log("++++++++++++++++++", l);
-    }
-    )
-}
-
   // fetch = async () => {
   //   const documents = (await getDocs(collection(db, 'projects'))).docs.map((doc) => doc.data());
   //   const json = new Blob([JSON.stringify(documents, null, 4)], { type: 'application/json' });
