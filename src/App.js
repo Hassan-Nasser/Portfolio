e45c70524633f073cf821f28a403e5945c1c371b
aya-abdullah <ayalaban888@yahoo.com>
2022-10-11 19:03:01 +0200

    UPDATE

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter } from "react-router-dom";
 import { AppContextProvider } from "./components/AppContext";
 import { Navigation } from "./components/Navigation/Navigation";
 import { Routes } from "./components/Routes";
+import {db} from "./config/firebase";
 
 function App() {
   return (
